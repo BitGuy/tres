dfef44dd3fbe57cd1a003584f80b33c9d2145766
bitguy <2653489791@qq.com>
2015-10-16 11:31:14 +0800

    6776

---
+++ ipeople/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ipeople
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mypoint mp = new mypoint(35, 160);
+            print(mp);
+            Console.Read();
+        }
+        private static void print(ipeople ip)
+        {
+            Console.WriteLine("体重:{0},身高:{1}\n", ip.w, ip.h);
+        }
+    }
+    public interface ipeople
+    {
+        int w
+        {
+            get;
+            set;
+        }
+        int h
+        {
+            get;
+            set;
+        }
+    }
+    class mypoint : ipeople
+    {
+        private int myw;
+        private int myh;
+        public mypoint(int w, int h)
+        {
+            myw = w;
+            myh = h;
+        }
+        public int w
+        {
+            get { return myw; }
+            set { myw = value; }
+        }
+        public int h
+        {
+            get { return myh; }
+            set { myh = value; }
+        }
+    }
+}
---
+++ testInterface/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            狗 旺财 = new 狗();
+            旺财.叫();
+            猫 麦 = new 猫();
+            麦.叫();
+            Console.ReadKey();
+        }
+    }
+    public interface 动物叫
+    {
+        void 叫();
+    }
+    public class 狗 : 动物叫
+    {
+        public void 叫()
+        {
+            Console.WriteLine("汪!汪!");
+        }
+    }
+    public class 猫 : 动物叫
+    {
+        public void 叫()
+        {
+            Console.WriteLine("喵喵!");
+        }
+    }
+}
